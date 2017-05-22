a3a4326165bfbabcfeec800163b18ea7a546853e
Jonnathon McCoy <jmccoy11@mail.greenriver.edu>
2017-05-21 20:10:21 -0500

    Commenting complete on blogger and blog-post

--- classes/blog-post.php
+++ classes/blog-post.php
@@ -1,13 +1,56 @@
 <?php
-
+  /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+  
+  /**
+   * File defines the class for creating Blog Post objects that contain
+   * the id, title, post, date posted, and word count.
+   *
+   * PHP version 5
+   *
+   * @category   CategoryName
+   * @package    PackageName
+   * @author     Jonnathon McCoy <jmccoy11@mail.greenriver.edu>
+   * @copyright  2017
+   * @version    1.1
+   */
+  
+  /**
+   * Class for creating a Blog Post
+   *
+   * Blog Posts will consist of the blog Id, the title of the Blog, the
+   * actual post from the user, the date it was posted, and its word
+   * count which is populated on creation of a BlogPost object.
+   *
+   * @category   CategoryName
+   * @package    PackageName
+   * @author     Jonnathon McCoy <jmccoy11@mail.greenriver.edu>
+   * @copyright  2017
+   * @version    1.1
+   */
 class BlogPost
 {
+  //{{{ properties
+ 
   private $_blogId,
     $_title,
     $_blogPost,
     $_datePosted,
     $_wordCount;
     
+    //}}}
+    
+    //{{{ __construct()
+    
+    /**
+     * Constructor for the BlogPost Class.
+     *
+     * @param int     $blogId     Blog Id Post in database
+     * @param string  $title      Blog title
+     * @param string  $blogPost   The User's post
+     * @param string  $datePosted  Date when posted to database
+     *
+     * @access public
+     */
     function __construct($blogId=0, $title="", $blogPost="", $datePosted="")
     {
       $this->_blogId = $blogId;
@@ -17,46 +60,123 @@ class BlogPost
       $this->_wordCount = str_word_count($blogPost);
     }
     
+    //}}}
+    
+    //{{{ getBlogId()
+    
+    /**
+     * Retrieve the Blog's Id number.
+     *
+     @return int  The blog Id in the database
+     @access public
+     */
     function getBlogId()
     {
       return $this->_blogId;
     }
     
-    function setBlogId($blogId)
-    {
-      $this->_blogId = $blogId;
-    }
+    //}}}
     
+    //{{{ getTitle()
+    
+    /**
+     *  Getter for the title.
+     *  
+     * @return string  Blog title
+     * @access public
+     */
     function getTitle()
     {
       return $this->_title;
     }
     
+    //}}}
+    
+    //{{{ setTitle()
+    
+    /**
+     * Setter for title
+     *
+     * @param string  $title  new title
+     * @access public
+     */
     function setTitle($title)
     {
       $this->_title = $title;
     }
     
+    //}}}
+    
+    //{{{ getPost()
+    
+    /**
+     * Getter for the post data
+     *
+     * @return  string  the user's post
+     * @access public
+     */ 
     function getPost()
     {
       return $this->_blogPost;
     }
     
+    //}}}
+    
+    //{{{ getPost()
+    
+    /**
+     * Setter for Post
+     *
+     * @param  string  $blogPost  New data for post
+     * @access public
+     */
     function setPost($blogPost)
     {
       $this->_blogPost = $blogPost;
     }
     
+    //}}}
+    
+    //{{{ getDatePosted()
+    
+    /**
+     * Getter for the date blog was posted.
+     *
+     * @return  string  Date posted. From database this
+     * is formated as YYYY-mm-dd
+     * @access public
+     */
     function getDatePosted()
     {
       return $this->_datePosted;
     }
     
+    //}}}
+    
+    //{{{ setDatePosted()
+    
+    /**
+     * Setter for date posted
+     *
+     * @param string  $datePosted   New date for posted. Will update if
+     * blog has been edited. Format needs to be YYY-mm-dd
+     * @access pulic
+     */
     function setDatePosted($datePosted)
     {
       $this->_datePosted = $datePosted;
     }
     
+    //}}}
+    
+    //{{{ getWordCount
+    
+    /**
+     * Getter for Word Count.
+     *
+     @return  int returns the word count of the blog post
+     @access public
+     */
     function getWordCount()
     {
       return $this->_wordCount;
--- classes/blogger.php
+++ classes/blogger.php
@@ -1,111 +1,310 @@
 <?php
-
-class Blogger
-{
-  private
-    $_bloggerId,
-    $_username,
-    $_name,
-    $_blogCount,
-    $_mostRecentBlogId,
-    $_mostRecentBlogDate,
-    $_profilePicPath,
-    $_bio;
+  /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+  
+  /**
+   * File defines the class for creating Blogger objects
+   *
+   * File defines the class for creating Blogger objects that contain
+   * the bloggerId, username, name, blog count, most recent blog id,
+   * the most recent blog date, filepath for the profile picture, and
+   * the biography.
+   *
+   * PHP version 5
+   *
+   * @category   CategoryName
+   * @package    PackageName
+   * @author     Jonnathon McCoy <jmccoy11@mail.greenriver.edu>
+   * @copyright  2017
+   * @version    1.1
+   */
+  
+  /**
+   * Class for creating Blogger objects
+   *
+   * Class for creating Blogger objects that contain
+   * the bloggerId, username, name, blog count, most recent blog id,
+   * the most recent blog date, filepath for the profile picture, and
+   * the biography.
+   *
+   * @category   CategoryName
+   * @package    PackageName
+   * @author     Jonnathon McCoy <jmccoy11@mail.greenriver.edu>
+   * @copyright  2017
+   * @version    1.1
+   */
+  class Blogger
+  {
+    //{{{ properties
     
-  private $_posts=array();
-  
-  function __construct($bloggerId=0, $username="", $name="", $blogCount=0,
-                       $mostRecent=0, $mostRecentBlogDate = "0000-00-00", $profilePic="", $bio="")
-  {
+    private
+      $_bloggerId,
+      $_username,
+      $_name,
+      $_blogCount,
+      $_mostRecentBlogId,
+      $_mostRecentBlogDate,
+      $_profilePicPath,
+      $_bio;
+      
+    private $_posts=array();
     
-    $this->_bloggerId = $bloggerId;
-    $this->_username = $username;
-    $this->_name = $name;
-    $this->_blogCount = $blogCount;
-    $this->_mostRecentBlogId = $mostRecent;
-    $this->_mostRecentBlogDate = $mostRecentBlogDate;
-    $this->_profilePicPath = $profilePic;
-    $this->_bio = $bio;
+    //}}}
     
+    //{{{ __construct()
+    
+    /**
+     * Constructor for the Blogger Class.
+     *
+     * @param int     $bloggerId            Blogger Id from the database
+     * @param string  $username             Blogger's username
+     * @param string  $age                  Blogger's name
+     * @param int     $blogCount            How many blogs the Blogger has made
+     * @param int     $mostRecent           The Id of the most recent blog the Blogger has made
+     * @param string  $mostRecentBlogDate   The date the most recent blog was made
+     * @param string  $profilePic           The path name for the profile picture
+     * @param bio     $bio                  The biography of the blogger
+     * @access public
+     */  
+    function __construct($bloggerId=0, $username="", $name="", $blogCount=0,
+                         $mostRecent=0, $mostRecentBlogDate = "0000-00-00", $profilePic="",
+                         $bio="")
+    {
+      
+      $this->_bloggerId = $bloggerId;
+      $this->_username = $username;
+      $this->_name = $name;
+      $this->_blogCount = $blogCount;
+      $this->_mostRecentBlogId = $mostRecent;
+      $this->_mostRecentBlogDate = $mostRecentBlogDate;
+      $this->_profilePicPath = $profilePic;
+      $this->_bio = $bio;
+      
+    }
+    
+    //}}}
+    
+    //{{{ getId()
+    
+    /**
+     * Getter for bloggerId.
+     *
+     * @return int  The blogger Id from the database
+     * @access public
+     */
+    function getId()
+    {
+      return $this->_bloggerId;
+    }
+    
+    //}}}
+    
+    //{{{ getUsername()
+    
+    /**
+     * Getter for username
+     *
+     * @return string   The blogger's username.
+     * @access public
+     */    
+    function getUsername()
+    {
+      return $this->_username;
+    }
+    
+    //}}}
+    
+    //{{{ getName()
+    
+    /**
+     * Getter for name
+     *
+     * @return string   Blogger name.
+     * @access public
+     */
+    function getName()
+    {
+      return $this->_name;
+    }
+    
+    //}}}
+    
+    //{{{ getBlogCount()
+    
+    /**
+     * Getter for blog count.
+     *
+     * @return int    How many blogs this blogger has made
+     * @access public
+     */    
+    function getBlogCount()
+    {
+      return $this->_blogCount;
+    }
+    
+    //}}}
+    
+    //{{{incrementBlogCount()
+    
+    /**
+     * Increment Blog count by one
+     *
+     * @access public
+     */
+    function incrementBlogCount()
+    {
+      $this->_blogCount++;
+    }
+    
+    //}}}
+    
+    //{{{ decrementBlogCount
+    
+    /**
+     * Decrease blog count by one
+     *
+     * @access public
+     */
+    function decrementBlogCount()
+    {
+      $this->_blogCount--;
+    }
+    
+    //}}}
+    
+    //{{{ getMostRecent()
+    
+    /**
+     * Get the blogId for the most recent post made by this blogger
+     *
+     * @return int    the blog id for the most recent post made by this blogger
+     * @access public
+     */    
+    function getMostRecent()
+    {
+      return $this->_mostRecentBlogId;
+    }
+    
+    //}}}
+    
+    //{{{ setMostRecent
+    
+    /**
+     * Setter the most recent post made by this blogger to the id passed to the function
+     *
+     * @param int   Blog id for the most recent post made by this blogger
+     * @access public
+     */
+    function setMostRecent($mostRecentBlogId)
+    {
+      $this->_mostRecentBlogId = $mostRecentBlogId;
+    }
+    
+    //}}}
+    
+    //{{{ getMostRecentBlogDate()
+    
+    /**
+     * Getter the date for the most recent blog made. Format is returned as YYYY-mm-dd.
+     *
+     * @return string   The date for the most recent post made by this blogger
+     * retruns as a string with the following format 'YYYY-mm-dd'
+     *@access public
+     */
+    function getMostRecentBlogDate()
+    {
+      return $this->_mostRecentBlogDate;
+    }
+    
+    //}}}
+    
+    //{{{ getPath()
+    
+    /**
+     * Getter for the file path for the blogger's profile picture
+     *
+     * @return string   The file path for the blogger's profile pic
+     * @access public
+     */
+    function getPath()
+    {    
+      return $this->_profilePicPath;
+    }
+    
+    //}}}
+    
+    //{{{ setPath()
+    
+    /**
+     * Setter for the file path for the blogger's profile picture
+     *
+     * @param string  filepath name for the blogger's profile picture
+     * @access public
+     */
+    function setPath($profilePicPath)
+    {
+      $this->_profilePicPath = $profilePicPath;
+    }
+    
+    //}}}
+    
+    //{{{ getBio()
+    
+    /**
+     * Getter for the blogger's biography
+     *
+     * @return  string    The blogger's biography
+     * @access public
+     */
+    function getBio()
+    {
+      return $this->_bio;
+    }
+    
+    //}}}
+    
+    //{{{ setBio()
+    
+    /**
+     * Setter for the blogger's biography
+     *
+     * @param string   The new blogger's biography
+     */
+    function setBio($bio)
+    {
+      $this->_bio = $bio;
+    }
+    
+    //}}}
+    
+    //{{{ getPostsArray()
+    
+    /**
+     * Getter for the posts array that stores all the posts this
+     * blogger has made (yes, terrible design, I know but this is
+     * a toy project)
+     *
+     * @return Array  Array of posts made by this blogger
+     * @access public
+     */
+    function getPostsArray()
+    {
+      return $this->_posts;
+    }
+    
+    //}}}
+    
+    //{{{ addPost()
+    
+    /**
+     * Add post to the posts array
+     *
+     * @param BlogPost  Blog post to push to the posts array.
+     * @access public
+     */
+    function addPost($blogPost)
+    {
+      array_push($this->_posts, $blogPost);
+    }
+    
+    //}}}
   }
-  
-  function getId()
-  {
-    return $this->_bloggerId;
-  }
-  
-  function setId()
-  {
-    return $this->_bloggerId;
-  }
-  
-  function getUsername()
-  {
-    return $this->_username;
-  }
-  
-  function getName()
-  {
-    return $this->_name;
-  }
-  
-  function getBlogCount()
-  {
-    return $this->_blogCount;
-  }
-  
-  function incrementBlogCount()
-  {
-    $this->_blogCount++;
-  }
-  
-  function decrementBlogCount()
-  {
-    $this->_blogCount--;
-  }
-  
-  function getMostRecent()
-  {
-    return $this->_mostRecentBlogId;
-  }
-  
-  function setMostRecent($mostRecentBlogId)
-  {
-    $this->_mostRecentBlogId = $mostRecentBlogId;
-  }
-  
-  function getMostRecentBlogDate()
-  {
-    return $this->_mostRecentBlogDate;
-  }
-  
-  function getPath()
-  {    
-    return $this->_profilePicPath;
-  }
-  
-  function setPath($profilePicPath)
-  {
-    $this->_profilePicPath = $profilePicPath;
-  }
-  
-  function getBio()
-  {
-    return $this->_bio;
-  }
-  
-  function setBio($bio)
-  {
-    $this->_bio = $bio;
-  }
-  
-  function getPostsArray()
-  {
-    return $this->_posts;
-  }
-  
-  function addPost($blogPost)
-  {
-    array_push($this->_posts, $blogPost);
-  }
-}
